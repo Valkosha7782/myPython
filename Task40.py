a00ddd3738f67dd5b93fec90e933858d4e46170a
Valentina Koshkina <122551688+Valkosha7782@users.noreply.github.com>
2023-06-07 15:50:45 +0300

    Create Task40.py
    Task40

---
+++ Task40.py
@@ -0,0 +1,6 @@
+#Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data. Определить среднюю стоимость дома, 
+#где кол-во людей от 0 до 500 (population).
+
+df = pd.read_csv('sample_data/california_housing_train.csv')
+
+print(df.loc[df.population <= 500, ['median_house_value']].mean())
